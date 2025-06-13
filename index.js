aa6ffd32cfeec7ee1a0f20a9e6701e9c9c7b1c7c
“daniel-bailey” <dan.p.bailey@bbc.co.uk>
2025-06-13 16:21:34 +0100

    Simple crunch and banner function

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-export function initCrunch() {
-  console.log('initCrunch called!');
-  
-  window.addEventListener("load", (event) => { 
-    console.log('page loaded and cookies crunched!');
-    //crunch will be called here and in other eventListeners
-  });
+//const fs = require('node:fs');
+const defaultTranslations = {
+  headerText: 'Cookies on the BBC website',
+  bannerText: 'Example banner text in here that will contain links to the settings page'
 
-  addEventListener("unload", (event) => { 
-    console.log('window unloaded and cookies crunched!');
-  })
+}; //import this from the en.json as a default
+
+export const crunch = () => {
+  console.log('cookie crunch called!');
 }
+
+export const showBanner = () => {
+  const languages = ['en', 'cy', 'ga', 'gd']; //there will be plenty more in here...
+
+  // We will have a folder of translation files, probably JSON or something called en.json, cy.json etc.
+  // roughly how translations would work...
+  /* if (language) {
+    fs.readFile(`/translations/${language}.json`, 'utf8', (err, data) => {
+      if (err) {
+        //default en.json
+        bannerText = defaultTranslations
+      } else {
+        bannerText = data;
+      }
+    });
+  } else {
+    bannerText = defaultTranslations;
+  } */
+
+  let bannerText = defaultTranslations;
+  const banner = `
+    <div class="cookie-banner">
+      <div class="container">
+        <div class="cookie-banner-content">
+          <div class="cookies-banner-text">
+            <h2>${bannerText.headerText}</h2>
+            <p>${bannerText.bannerText}</p>
+          </div>
+          <div class="cookies-banner-buttons">
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Go to settings</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.body.appendChild(banner)
+};
