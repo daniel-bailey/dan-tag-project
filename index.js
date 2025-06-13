0aa38161f06fa8bca39986157478077e855c19d8
“daniel-bailey” <dan.p.bailey@bbc.co.uk>
2025-06-13 12:01:38 +0100

    Add more eventlistener stuff

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 export function initCrunch() {
+  console.log('initCrunch called!');
+  
   window.addEventListener("load", (event) => { 
     console.log('page loaded and cookies crunched!');
     //crunch will be called here and in other eventListeners
   });
+
+  addEventListener("unload", (event) => { 
+    console.log('window unloaded and cookies crunched!');
+  })
 }
