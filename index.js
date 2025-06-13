07aeadecb7f547f0f0ff625b51eff5a13daf7f1d
“daniel-bailey” <dan.p.bailey@bbc.co.uk>
2025-06-13 11:53:46 +0100

    Create function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", (event) => { 
-  console.log('page loaded and cookies crunched!');
-  //crunch will be called here and in other eventListeners
-});
+export function initCrunch() {
+  window.addEventListener("load", (event) => { 
+    console.log('page loaded and cookies crunched!');
+    //crunch will be called here and in other eventListeners
+  });
+}
